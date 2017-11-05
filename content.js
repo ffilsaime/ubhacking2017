5cb8333d8294a6d0f83c030d32134001001fe082
Florebencia Fils-Aime <floreben@buffalo.edu>
2017-11-04 23:12:46 -0400

    Collector of HTML

--- content.js
+++ content.js
@@ -3,3 +3,6 @@ alert("Test!1");
 var firstHref = $("a[href^='http']").eq(0).attr("href");
 console.log("test");
 alert(firstHref);
+function getPageHTML(){
+	return "<html>" + $("html").html() + "</html>";
+}
